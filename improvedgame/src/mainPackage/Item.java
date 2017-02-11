3dcc040911932c68a4d7c7dd99475b523285afea
Ganesh Govind <Ganesh Govind@LAPTOP-1234.attlocal.net>
2017-02-11 08:10:59 -0800

    removed the other classes. errors

--- improvedgame/src/mainPackage/Item.java
+++ improvedgame/src/mainPackage/Item.java
@@ -10,12 +10,14 @@ public class Item {
 	
 	//generator
 	//generators generate power for the ship. They are needed by weapons, thrusters, and shields
+	public boolean IsGenerator = false;
 	public double PowerGenerated;//the points of power generated each tick
 	public double CurrentPower;//the current amount of power stored
 	public double MaxPower;//the max amount of power stored by the generator
 	
 	//shield
 	//shields absorb some percentage of the damage. When the shields' own health drops below zero, they fail for a period of time
+	public boolean IsShield = false;
 	public double DamageMultiplier;//the number the damage is multiplied by. Lower is better
 	public double MaxHealth;//the max amount of health the shield can have
 	public double CurrentHealth;//the current amount of health the shield has
@@ -26,22 +28,26 @@ public class Item {
 	
 	//Thruster
 	//thrusters propel the ship, drawing power
+	public boolean IsThruster = false;
 	public double Acceleration;//the acceleration, affected by mass
 	public double PowerConsumptionPerThrust;
 	
 	//Weapon
 	//weapons allow the ship to fire at enemies or asteroids
-	public String EntityFired;//
+	public boolean IsWeapon = false;
+	public String EntityFired;//the actual object launched
 	public double Velocity; // the velocity of the entity
 	public double Spread; // the spread in radians
 	public double DamageBoost;// the damage multiplier
 	public int NumberFiredAtOnce;//bullets fired at once
-	public int MaxWeaponReload;//Max reload level
-	public int CurrentWeaponReload;//the current reload
-	public int WeaponRegeneration; //how fast it reloads
+	public double MaxWeaponReload;//Max reload level
+	public double CurrentWeaponReload;//the current reload
+	public double WeaponRegeneration; //how fast it reloads
+	public double PowerConsumptionWhenFired;//how much energy is consumed
 	
 	public void InitializeGenerator(double powergen, double max)
 	{
+		IsGenerator = true;
 		PowerGenerated = powergen;
 		MaxPower = max;
 		CurrentPower = max;
@@ -49,12 +55,37 @@ public class Item {
 	
 	public void InitializeShield(double dammult, double max, double regen, double fixtime, double powersump)
 	{
+		IsShield = true;
 		DamageMultiplier = dammult;
 		MaxHealth = max;
+		CurrentHealth = max;
+		RegenPerTick = regen;
+		FixTime = fixtime;
+		CurrentFix = 0;
+		PowerConsumptionPerRegenTick = powersump;
 		
 	}
-	public void InitializeShield(DamageMultiplier )
 	
+	public void InitializeThruster(double accel, double powersump)
+	{
+		IsThruster = true;
+		Acceleration = accel;
+		PowerConsumptionPerThrust = powersump;
+	}
+	
+	public void InitializeWeapon(String type, double vel, double spread, double boost, int atonce, double maxrel, double regen, double powersump)
+	{
+		IsWeapon = true;
+		EntityFired = type;
+		Velocity = vel;
+		Spread = spread;
+		DamageBoost = boost;
+		NumberFiredAtOnce= atonce;
+		MaxWeaponReload = maxrel;
+		CurrentWeaponReload = maxrel;
+		WeaponRegeneration = regen;
+		PowerConsumptionWhenFired = powersump;
+	}
 	public Item(String name, String description, String type, Image sprite)
 	{
 		Name = name;
